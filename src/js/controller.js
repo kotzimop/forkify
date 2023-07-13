9247662ffb93fff9c78c0bf3f942ed9cd3239da2
kotzimop <kotzimop@gmail.com>
2023-07-13 08:08:13 +0300

    Final Code

---
+++ src/js/controller.js
@@ -0,0 +1,55 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+///////////////////////////////////////
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    // slice(1) removes the # symbol
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    // console.log(id);
+    recipeView.renderSpinner();
+
+    // 1. Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) render results
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+
+init();
